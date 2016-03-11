14c1d08970cc628ef066df0c34b59f0cdbc5db00
koliaok <Lak@ACC880E6.ipt.aol.com>
2016-03-11 14:11:53 +0900

    programing

--- press.c
+++ press.c
@@ -2,9 +2,16 @@
 
 int main(int argc, const char * argv[]) {
     int k=0;
-    scanf("%d",&k);
+   
+    while(1)
+{
+
+     scanf("%d",&k);
     
-    if(k==1) printf("jikuaij@naver.com ");
-    else printf("don't plz press 1");
-    return 0;
+    if(k==1) printf("jikuaij@naver.com \n");
+    else if(k==2) printf("010-5055-3835\n");
+    else if(k==3) return 0;   
+    else printf("don't plz press 1 or 2");
+}
+return 0;
 }
