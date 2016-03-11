31cd8ba2d577d4006795d82033f861d159a37f3a
koliaok <Lak@ACC880E6.ipt.aol.com>
2016-03-11 13:41:02 +0900

    myfistgit

---
+++ press.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int k=0;
+    scanf("%d",&k);
+    
+    if(k==1) printf("jikuaij@naver.com ");
+    else printf("don't plz press 1");
+    return 0;
+}
